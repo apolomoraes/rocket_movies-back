bb246c9af3c338772f93be37fe064fabe8937165
Apolo Moraes <vlogsapolo@gmail.com>
2023-03-15 14:59:29 -0300

    adicionado cors

--- src/server.js
+++ src/server.js
@@ -2,14 +2,19 @@ require("express-async-errors");
 
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
-const express = require("express");
+const uploadConfig = require("./configs/upload");
 
+const cors = require("cors");
+const express = require("express");
 const routes = require("./routes");
 
 migrationsRun();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 
